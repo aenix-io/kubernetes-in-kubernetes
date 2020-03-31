4b01f3edf81a221ed43b2b741349c347623e4b99
kvaps <kvapss@gmail.com>
2020-03-31 20:48:21 +0200

    add basic values

--- deploy/helm/kubernetes/scripts/configure-cluster.sh
+++ deploy/helm/kubernetes/scripts/configure-cluster.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 set -e
+set -x
+
+# ------------------------------------------------------------------------------
+# Setup environment
+# ------------------------------------------------------------------------------
 
 mkdir -p /etc/kubernetes/pki
 ln -sf /pki/apiserver-etcd-client/tls.crt /etc/kubernetes/pki/apiserver-etcd-client.crt
@@ -15,6 +20,9 @@ ln -sf /pki/front-proxy-ca/tls.crt /etc/kubernetes/pki/front-proxy-ca.key
 ln -sf /pki/front-proxy-client/tls.key /etc/kubernetes/pki/front-proxy-client.crt
 ln -sf /pki/front-proxy-client/tls.crt /etc/kubernetes/pki/front-proxy-client.key
 
+# ------------------------------------------------------------------------------
+# Update secrets and component configs
+# ------------------------------------------------------------------------------
 
 cat > kubeadmcfg.yaml << EOT
 apiVersion: "kubeadm.k8s.io/v1beta2" 
@@ -46,14 +54,35 @@ until kubectl --kubeconfig /etc/kubernetes/admin.conf cluster-info >/dev/null 2>
   sleep 1
 done
 
-# upload configuration
-kubeadm --kubeconfig /etc/kubernetes/admin.conf init phase upload-config kubeadm
+# ------------------------------------------------------------------------------
+# Cluster configuration
+# ------------------------------------------------------------------------------
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
-kubeadm --kubeconfig /etc/kubernetes/admin.conf init phase upload-config kubelet -v1 2>&1 \
+# upload configuration
+kubeadm init phase upload-config kubeadm --config /config/kubeadmcfg.yaml
+kubectl --kubeconfig /etc/kubernetes/admin.conf patch configmap -n kube-system kubeadm-config -p '{"data":{"ClusterStatus":"apiEndpoints: {}\napiVersion: kubeadm.k8s.io/v1beta2\nkind: ClusterStatus"}}'
+
+# upload configuration
+kubeadm init phase upload-config kubelet --config /config/kubeadmcfg.yaml -v1 2>&1 \
   | while read line; do echo "$line" | grep 'Preserving the CRISocket information for the control-plane node' && killall kubeadm || echo "$line"; done
 
-kubeadm --kubeconfig /etc/kubernetes/admin.conf init phase bootstrap-token
+# setup bootstrap-tokens
+if ! kubectl --kubeconfig /etc/kubernetes/admin.conf -n kube-public get cm cluster-info >/dev/null 2>&1; then
+  kubeadm init phase bootstrap-token --config /config/kubeadmcfg.yaml --skip-token-print
+fi
 
-kubeadm --kubeconfig /etc/kubernetes/admin.conf init phase addon coredns
+# correct apiserver address for the external clients
+if [ -n "$CONTROL_PLANE_ENDPOINT" ]; then
+  tmp="$(mktemp -d)"
+  kubectl --kubeconfig /etc/kubernetes/admin.conf get configmap -n kube-public cluster-info -o jsonpath='{.data.kubeconfig}' > "$tmp/kubeconfig"
+  kubectl --kubeconfig "$tmp/kubeconfig" config set clusters..server "https://${CONTROL_PLANE_ENDPOINT}"
+  kubectl create configmap cluster-info --from-file="$tmp/kubeconfig" --dry-run=client -o yaml | kubectl --kubeconfig /etc/kubernetes/admin.conf apply -n kube-public -f -
+  rm -rf "$tmp"
+fi
 
-kubeadm --kubeconfig /etc/kubernetes/admin.conf init phase addon kube-proxy
+# install cordns
+kubeadm init phase addon coredns --config /config/kubeadmcfg.yaml
+
+# install kube-proxy
+kubeadm init phase addon kube-proxy --config /config/kubeadmcfg.yaml
